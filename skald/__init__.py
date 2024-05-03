3f624fbab4e341e8465aa1b11fcbffb5d1990c3a
Farthofer Laurenz (KAI DSC) <Laurenz.Farthofer@k-ai.at>
2024-05-03 16:11:36 +0200

    🐍 started work on Logger class

--- skald/__init__.py
+++ skald/__init__.py
@@ -2,3 +2,26 @@
 
 📟 a simple and efficient experiment logger for Python 🐍
 """
+
+from pathlib import Path
+import beartype
+from loguru import logger
+
+
+@beartype
+class Logger:
+    dir: Path
+    meta_file: Path
+    metrics_file: Path
+    params_file: Path | None
+    artifacts_dir: Path
+
+    def __init__(
+        self,
+        dir: Path = Path("."),
+        meta_file: Path = Path("meta.yaml"),
+        metrics_file: Path = Path("metrics.parquet"),
+        params_file: Path | None = None,
+        artifacts_dir: Path = Path("artifacts"),
+    ) -> None:
+        logger.info(f"📂 logging to {dir}")
